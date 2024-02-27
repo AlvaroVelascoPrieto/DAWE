3ae6fa62b4a5cc00d4534d3b4ad7a3a31bda0cfb
AlvaroVelascoPrieto <alvaritovelasco2002@gmail.com>
2024-02-27 12:51:36 +0100

    Fixed adding multiple items to cart, cart is now scrollable. Cart elements have a hover animation

--- Entrega 4/js/main.js
+++ Entrega 4/js/main.js
@@ -85,20 +85,22 @@ window.onload = () => {
             var quantity = parseInt(productCard.querySelector('.cantidad-spinner').value);
 
             var carrito = document.getElementById('Carrito');
-            carrito.innerHTML = `<h2>Carrito</h2>`;
             carrito.style.display = 'block';
+            
 
             var carro = document.getElementById('Carro');
             carro.style.width = '20%';
-            carrito.style.position = 'fixed';
             
-
+        
             var tienda = document.getElementById('Tienda');
             tienda.style.width = '80%';
 
+            var divProducto = document.createElement('div')
             var productoEnCarrito = document.createElement('p');
             productoEnCarrito.textContent = `${productBrand} ${productName}, Cantidad: ${quantity}`;
-            carrito.appendChild(productoEnCarrito);
+            divProducto.className = 'productoEnCarrito'
+            divProducto.append(productoEnCarrito)
+            carrito.appendChild(divProducto);
 
             
         });
